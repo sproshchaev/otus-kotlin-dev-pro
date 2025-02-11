3df0b53f9639948fdc18b62c9754c52a650e9286
sproshchaev <sproshchaev@gmail.com>
2025-02-11 22:33:20 +0500

    add example

--- kotlin-app/src/main/kotlin/com/prosoft/Main.kt
+++ kotlin-app/src/main/kotlin/com/prosoft/Main.kt
@@ -1,8 +1,31 @@
 package com.prosoft
 
 /**
- * kotlin-app (maven)
+ * Практика создания DSL в Kotlin (11-02-2025)
  */
 fun main() {
-    println("Hello World!")
+
+    val user = user {
+        "name" to "John Doe"
+        "age" to 30
+        "email" to "john.doe@example.com"
+    }
+
+    println(user)
+
+    val user2 = user {
+        name = "Richard Roe"
+        age = 32
+        email = "richard.roe@example.com"
+    }
+
+    println(user2)
+
+    val (userName, userAge, userEmail) = user
+    println("Name: $userName, Age: $userAge, Email: $userEmail")
+
+    println("User name: ${user["name"]}")
+    user["email"] = "new.email@example.com"
+    println("Updated email: ${user.email}")
+
 }
