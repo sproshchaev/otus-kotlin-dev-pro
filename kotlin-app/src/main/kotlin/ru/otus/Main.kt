28ebd359b719b94ac8ea22ce996be3f117d7e1b8
sproshchaev <sproshchaev@gmail.com>
2025-08-12 00:43:49 +0700

    add example

--- kotlin-app/src/main/kotlin/ru/otus/Main.kt
+++ kotlin-app/src/main/kotlin/ru/otus/Main.kt
@@ -6,8 +6,5 @@ import ru.otus.dataclass.Person
  * kotlin-app (maven) 12-08-2025 Otus
  */
 fun main() {
-    val person = Person("Jhon", 15)
-    val person2 = Person("Jhon", 15)
-
-    println(person == person2)
+    println("Hello, Kotlin!")
 }
